71dd4f65b031e81bcf125885a295ba3ca50eb29b
Shreya <shreyathummar71@gmail.com>
2024-09-12 15:42:31 +0200

    homepage, header and pokemoncard component finish

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import {
 import MainLayout from "./components/MainLayout";
 import "./App.css";
 import Homepage from "./components/Homepage";
+import AllPokemon from "./components/AllPokemon";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
+        <Route index element={<AllPokemon />} />
         <Route path="/" element={<Homepage />} />
-        <Route path="/water" element={<Homepage />} />
       </Route>
     )
   );
